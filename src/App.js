516016d8c707df9c9cae644c71ea10e17249f75a
redwanX <redwanistbd2@gmail.com>
2022-04-17 00:23:03 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Pages/Shared/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
+import Blog from './Pages/Blog/Blog';
+import About from './Pages/About/About';
+import PageNotFound from './Pages/Shared/PageNotFound/PageNotFound';
+import Login from './Pages/Authentication/Login/Login';
+import Register from './Pages/Authentication/Register/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/blog' element={<Blog></Blog>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='*' element={<PageNotFound></PageNotFound>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Container, Nav, Navbar} from 'react-bootstrap'
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <Navbar className='py-3' collapseOnSelect expand="lg" bg=" bg-teal-500 " variant="dark">
+  <Container>
+  <Navbar.Brand>ALEX - TRAVEL GUIDE</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className='ms-auto'>
+      <Nav.Link as={Link} to='about'>About</Nav.Link>
+      <Nav.Link as={Link} to='blog'>Blog</Nav.Link>
+      <Nav.Link as={Link} to='login'>Login</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+  )
+}
+
+export default Header
